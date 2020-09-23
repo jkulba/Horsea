c8cfedbfd2e096524c7495133e45a9665c5d5723
James Kulba <jkulba@gmail.com>
2020-09-22 18:40:34 -0700

    Added Test log configuration.

--- src/test/java/us/kulba/horsea/ApplicationTests.java
+++ src/test/java/us/kulba/horsea/ApplicationTests.java
@@ -2,15 +2,23 @@ package us.kulba.horsea;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import us.kulba.horsea.config.TestPersistenceConfig;
 
 @ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = TestPersistenceConfig.class)
 @SpringBootTest
 public class ApplicationTests {
 
+    private static final Logger log = LoggerFactory.getLogger(ApplicationTests.class);
+
     @Test
     public void contextLoads() {
+        log.info("Context Test");
     }
 
 }
